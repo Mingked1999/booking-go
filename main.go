275f1536582c9a4200103f64c616cb2bfd324cbc
Mingked1999 <118732687+Mingked1999@users.noreply.github.com>
2023-03-02 13:05:25 +1100

    cocunrrent and thread done

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"booking-app/helper"
 	"fmt" // pkg.go.dev/fmt
-	"strings"
+	"sync"
+	"time"
 )
 
 //2:37:20
 /*package level variables*/
 var conferenceName = "Go Conference"
 var remianingTickets uint = conferenceTickets
-var bookings []string // var bookings = [50]string{}
+
+// var bookings = make([]map[string]string, 0) // var bookings = [50]string{}
+var bookings = make([]UserData, 0) // var bookings = [50]string{}
 const conferenceTickets = 50
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
 
 func main() {
 	//fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remianingTickets, conferenceName)
@@ -19,34 +31,39 @@ func main() {
 	//greetUsers(conferenceName, conferenceTickets, remianingTickets)
 	greetUsers()
 
-	for remianingTickets > 0 && len(bookings) < 50 {
-		firstname, lastname, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidNumber := helper.ValidInputs(firstname, lastname, email, userTickets, remianingTickets)
+	//for remianingTickets > 0 && len(bookings) < 50 {
+	firstname, lastname, email, userTickets := getUserInput()
+	isValidName, isValidEmail, isValidNumber := helper.ValidInputs(firstname, lastname, email, userTickets, remianingTickets)
 
-		if isValidName && isValidEmail && isValidNumber {
-			bookTickets(userTickets, firstname, lastname, email)
-			firstNames := getFirstNames()
-			fmt.Printf("Thes first names of bookings are: %v\n", firstNames)
+	if isValidName && isValidEmail && isValidNumber {
+		bookTickets(userTickets, firstname, lastname, email)
 
-			if remianingTickets == 0 {
-				//end program
-				fmt.Println("Our Conference is booked out, come back next year")
-				break //stop loop
-			}
-		} else {
-			if !isValidName {
-				fmt.Println("first name or last name was too short")
-			}
-			if !isValidEmail {
-				fmt.Println("The email you entered didn't contain @ sign")
-			}
-			if !isValidNumber {
-				fmt.Println("please enter a valid number")
-			}
-			//fmt.Printf("There are only %v tickets left, so you can't book %v tickets\n", remianingTickets, userTickets)
-			//continue //loop again from start of loop
+		wg.Add(1) //wait before creating new thread
+		go sendTicket(userTickets, firstname, lastname, email)
+
+		firstNames := getFirstNames()
+		fmt.Printf("Thes first names of bookings are: %v\n", firstNames)
+
+		if remianingTickets == 0 {
+			//end program
+			fmt.Println("Our Conference is booked out, come back next year")
+			//break //stop loop
 		}
+	} else {
+		if !isValidName {
+			fmt.Println("first name or last name was too short")
+		}
+		if !isValidEmail {
+			fmt.Println("The email you entered didn't contain @ sign")
+		}
+		if !isValidNumber {
+			fmt.Println("please enter a valid number")
+		}
+		//fmt.Printf("There are only %v tickets left, so you can't book %v tickets\n", remianingTickets, userTickets)
+		//continue //loop again from start of loop
 	}
+	wg.Wait()
+	//}
 }
 
 func greetUsers() {
@@ -58,8 +75,8 @@ func greetUsers() {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings { //_ blink indentifier for where index is declare but not used
-		var names = strings.Fields(booking) //separate by space
-		firstNames = append(firstNames, names[0])
+		//var names = strings.Fields(booking) //separate by space
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -82,11 +99,35 @@ func getUserInput() (string, string, string, uint) {
 	fmt.Scan(&userTickets)
 	return firstname, lastname, email, userTickets
 }
+
 func bookTickets(userTickets uint, firstname string, lastname string, email string) {
 	remianingTickets = remianingTickets - userTickets
-	bookings = append(bookings, firstname+" "+lastname)
+	//create a map for user
+	//var userData = make(map[string]string)
+	var userData = UserData{
+		firstName:       firstname,
+		lastName:        lastname,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+	// userData["firstName"] = firstname
+	// userData["lastName"] = lastname
+	// userData["email"] = email
+	// userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
 
 	//slice in Go-> avstration of an array, variable-length
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receieve a confirmation email at %v\n", firstname, lastname, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remianingTickets, conferenceName)
+}
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("############################")
+	fmt.Printf("Sending ticket: \n %v \nto email address %v\n", ticket, email)
+	fmt.Println("############################")
+	wg.Done()
 }
--- readMe.md
+++ readMe.md
@@ -1,3 +1,12 @@
 ## startup
 
-go mod init booking-app1
+go mod init booking-app
+
+## checking points
+
+- declare variable var name string = "hello" -> name:= "hello"
+- export function -> Capitalise
+- map -> make([]map[string]string, 0)
+- type struct name{name string}
+- go functionName to concurrent
+- execute thread so main thread can be done WaitGroup.Add.Wait.Done
